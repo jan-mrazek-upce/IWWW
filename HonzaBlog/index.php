807b8a9d91874fcf76f88d43d7ba6b86f6106343
mrazek <st49666@student.upce.cz>
2019-01-11 17:38:42 +0100

    první nástřely

---
+++ HonzaBlog/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+</head>
+
+<body>
+<header><div id="header-web-title">Honza tech tips</div>
+    <nav id="main-nav">
+        <a href="#">Home</a>
+        <a href="#">About me</a>
+        <a href="#">Contact me</a>
+    </nav>
+</header>
+
+<main>
+    <nav id="tags-nav">
+
+        tady bude script co nacte vsechny temata
+
+    </nav>
+
+
+</main>
+
+<footer>
+
+</footer>
+
+</body>
+
+</html>
