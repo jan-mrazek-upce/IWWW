0e6d53483a8e0c36f802f5298b03aad9d9ee4bd3
mrazek <st49666@student.upce.cz>
2019-01-27 22:25:07 +0100

    message

--- HonzaBlog/index.php
+++ HonzaBlog/index.php
@@ -2,24 +2,66 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/layout.css">
+    <link rel="stylesheet" type="text/css" href="css/responsive.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Honza tech tips</title>
 
 </head>
 
 <body>
-<header><div id="header-web-title">Honza tech tips</div>
-    <nav id="main-nav">
-        <a href="#">Home</a>
-        <a href="#">About me</a>
-        <a href="#">Contact me</a>
-    </nav>
+
+
+<header>
+
+    <!--<div id="loging_window"></div>
+    -->
 </header>
 
 <main>
-    <nav id="tags-nav">
-
-        tady bude script co nacte vsechny temata
+    <nav id="tabs-holder">
+        <h3 class="categories">Kategorie</h3>
+        <ul>
+            <li><a href="#">Raspberry Pi</a></li>
+            <li><a href="#">DIY</a></li>
+            <li><a href="#">Intel</a></li>
+        </ul>
 
     </nav>
+
+    <div class="ar-holder">
+        <article>
+            <a class="ar-headline" href="#">Who I am</a>
+            <div class="ar-content">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Duis ante orci, molestie vitae vehicula
+                    venenatis,
+                    tincidunt ac pede. Fusce wisi. Praesent vitae arcu tempor neque lacinia pretium. Phasellus faucibus
+                    molestie
+                    nisl. Etiam quis quam. Duis sapien <strong>nunc</strong>, <i>commodo</i> et, <u>interdum</u>
+                    suscipit, sollicitudin et, dolor. Nam libero
+                    tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat
+                    facere
+                    possimus, omnis voluptas assumenda est, omnis dolor repellendus. Quisque tincidunt scelerisque
+                    libero.
+                    Aenean
+                    vel massa quis mauris vehicula lacinia. Mauris tincidunt sem sed arcu. Maecenas ipsum velit,
+                    consectetuer eu
+                    lobortis ut, dictum at dui. Vivamus ac leo pretium faucibus. Sed vel lectus. Donec odio tempus
+                    molestie,
+                    porttitor ut, iaculis quis, sem. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum
+                    dolore eu
+                    fugiat nulla pariatur.
+                </p>
+            </div>
+
+            <time>Vydano: 23. 11. 2005</time>
+        </article>
+
+    </div>
 
 
 </main>
---
+++ HonzaBlog/page/header.php
@@ -0,0 +1,11 @@
+<?php
+?>
+
+<div id="header-web-title">Honza tech tips</div>
+
+<nav id="main-nav">
+    <a href="">Home</a>
+    <a href="#">About me</a>
+    <a href="#">Contact me</a>
+</nav>
+
